90e84f6c5b90ae0a7694f82fbfe1497b6218e0bf
Phillip Alexander <phillip.alexander@galvanize.com>
2021-06-28 12:30:49 +0000

    (init)

---
+++ schema.sql
@@ -0,0 +1,11 @@
+/*  Execute this file from the command line by typing:
+ *    mysql -u <USER> < schema.sql
+ *    OR
+ *    mysql -u <USER> -p < schema.sql
+ *  For example, on a pairing station, it'll be
+ *    mysql -u student -p < schema.sql
+ *  and then you'll have to enter the password, student
+ *  On your personal computer, if you haven't set up
+ *  a password, it'll be
+ *    mysql -u root < schema.sql
+*/
